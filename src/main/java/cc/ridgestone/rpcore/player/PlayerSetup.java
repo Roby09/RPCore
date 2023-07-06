a8b228393abb8b5ec5360e7a29968770575e0e97
Roby09 <q.r.depreter@gmail.com>
2023-07-06 20:46:02 +0200

    refactor: use switch statement instead of if else for setup stage

--- src/main/java/cc/ridgestone/rpcore/player/PlayerSetup.java
+++ src/main/java/cc/ridgestone/rpcore/player/PlayerSetup.java
@@ -40,64 +40,64 @@ public class PlayerSetup implements Listener {
 
     @EventHandler
     public void onChat(AsyncPlayerChatEvent event) {
-        if(event.getPlayer().getUniqueId() != player.getUniqueId())
+        if (event.getPlayer().getUniqueId() != player.getUniqueId())
             return;
 
         event.setCancelled(true);
 
-        if (setupStage == 1) {
-            name = event.getMessage();
-            player.sendMessage(ChatColor.GREEN + "Successfully set your character's name to: " + ChatColor.BOLD + "" + name);
-            player.sendMessage(ChatColor.DARK_AQUA + "Please input your character's bio: ex (John Bill is tall.)");
-            sendSpace();
-            setupStage = 2;
-            return;
-        }
-        if (setupStage == 2) {
-            bio = event.getMessage();
-            player.sendMessage(ChatColor.GREEN + "Successfully your character's bio to: " + ChatColor.BOLD + "" + bio);
-            player.sendMessage(ChatColor.DARK_AQUA + "Please type your character's role: adult or student");
-            sendSpace();
-            setupStage = 3;
-            return;
-        }
-        if (setupStage == 3) {
-            if (event.getMessage().equalsIgnoreCase("adult")) {
-                student = false;
-                player.sendMessage(ChatColor.GREEN + "Successfully your character's role to: " + ChatColor.BOLD + "adult");
-                player.sendMessage(ChatColor.DARK_AQUA + "Please input your character's age: 20-80");
-            } else if (event.getMessage().equalsIgnoreCase("student")) {
-                student = true;
-                player.sendMessage(ChatColor.GREEN + "Successfully your character's role to: " + ChatColor.BOLD + "student");
-                player.sendMessage(ChatColor.DARK_AQUA + "Please input your character's age: [Year 1: 18-22] [Year 2: 19-23] [Year 3: 20-24] [Year 4: [21-26]");
-            } else {
-                player.sendMessage(ChatColor.RED + "Please type your character's role: adult or student");
-                return;
-            }
-            setupStage = 4;
-            sendSpace();
-            return;
-        }
-        if (setupStage == 4) {
-            try {
-                Integer.parseInt(event.getMessage());
-            } catch (NumberFormatException e) {
-                player.sendMessage(ChatColor.RED + "No numeric value found. Please only use numbers.");
-                return;
-            }
-            int age = Integer.parseInt(event.getMessage());
-            if (student && age < 18 || age > 26) {
-                player.sendMessage(ChatColor.RED + "A student can only be 18-26 years old");
-                return;
-            } else if (!student && age < 20 || age > 80) {
-                player.sendMessage(ChatColor.RED + "An adult can only be 20-80 years old");
-                return;
-            }
-            player.sendMessage(ChatColor.GREEN + "Successfully your character's age to: " + ChatColor.BOLD + "" + age);
-            sendSpace();
-            completableFuture.complete(new Character(name, bio, student, age, player.getLocation()));
-            player.sendMessage(ChatColor.GREEN + "Character " + name + " created successfully");
-            cancel();
+        switch (setupStage) {
+            case 1:
+                name = event.getMessage();
+                player.sendMessage(ChatColor.GREEN + "Successfully set your character's name to: " + ChatColor.BOLD + "" + name);
+                player.sendMessage(ChatColor.DARK_AQUA + "Please input your character's bio: ex (John Bill is tall.)");
+                sendSpace();
+                setupStage = 2;
+                break;
+            case 2:
+                bio = event.getMessage();
+                player.sendMessage(ChatColor.GREEN + "Successfully your character's bio to: " + ChatColor.BOLD + "" + bio);
+                player.sendMessage(ChatColor.DARK_AQUA + "Please type your character's role: adult or student");
+                sendSpace();
+                setupStage = 3;
+                break;
+
+            case 3:
+                if (event.getMessage().equalsIgnoreCase("adult")) {
+                    student = false;
+                    player.sendMessage(ChatColor.GREEN + "Successfully your character's role to: " + ChatColor.BOLD + "adult");
+                    player.sendMessage(ChatColor.DARK_AQUA + "Please input your character's age: 20-80");
+                } else if (event.getMessage().equalsIgnoreCase("student")) {
+                    student = true;
+                    player.sendMessage(ChatColor.GREEN + "Successfully your character's role to: " + ChatColor.BOLD + "student");
+                    player.sendMessage(ChatColor.DARK_AQUA + "Please input your character's age: [Year 1: 18-22] [Year 2: 19-23] [Year 3: 20-24] [Year 4: [21-26]");
+                } else {
+                    player.sendMessage(ChatColor.RED + "Please type your character's role: adult or student");
+                    return;
+                }
+                setupStage = 4;
+                sendSpace();
+                break;
+            case 4:
+                try {
+                    Integer.parseInt(event.getMessage());
+                } catch (NumberFormatException e) {
+                    player.sendMessage(ChatColor.RED + "No numeric value found. Please only use numbers.");
+                    return;
+                }
+                int age = Integer.parseInt(event.getMessage());
+                if (student && age < 18 || age > 26) {
+                    player.sendMessage(ChatColor.RED + "A student can only be 18-26 years old");
+                    return;
+                } else if (!student && age < 20 || age > 80) {
+                    player.sendMessage(ChatColor.RED + "An adult can only be 20-80 years old");
+                    return;
+                }
+                player.sendMessage(ChatColor.GREEN + "Successfully your character's age to: " + ChatColor.BOLD + "" + age);
+                sendSpace();
+                completableFuture.complete(new Character(name, bio, student, age, player.getLocation()));
+                player.sendMessage(ChatColor.GREEN + "Character " + name + " created successfully");
+                cancel();
+                break;
         }
     }
 
