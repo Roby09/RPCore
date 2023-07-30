d4be37d22b201912e3d46f30686d691d941a6684
Roby09 <q.r.depreter@gmail.com>
2023-07-30 21:44:25 +0200

    fix: double bio message (added cooldown)

--- src/main/java/cc/ridgestone/rpcore/listener/BioListener.java
+++ src/main/java/cc/ridgestone/rpcore/listener/BioListener.java
@@ -1,24 +1,38 @@
 package cc.ridgestone.rpcore.listener;
 
 import cc.ridgestone.rpcore.RPCore;
+import cc.ridgestone.rpcore.config.Variable;
 import cc.ridgestone.rpcore.player.Character;
 import cc.ridgestone.rpcore.player.RPPlayer;
+import me.clip.placeholderapi.PlaceholderAPI;
+import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerInteractAtEntityEvent;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
+
 public class BioListener implements Listener {
+
+    private List<UUID> cooldown = new ArrayList<UUID>();
 
     @EventHandler
     public void playerInteractAtPlayer(PlayerInteractAtEntityEvent event) {
         if (event.getRightClicked() instanceof Player player) {
             if (event.getPlayer().isSneaking()) {
                 RPPlayer rpPlayer = RPCore.i.getPlayerManager().getRpPlayer(player.getUniqueId());
-                if (rpPlayer != null) {
+                if (rpPlayer != null && !cooldown.contains(event.getPlayer().getUniqueId())) {
+                    String format = ChatColor.translateAlternateColorCodes('&', Variable.BIO_FORMAT.getValue());
+                    String finalFormat = PlaceholderAPI.setPlaceholders(player, format);
+
                     Character character = rpPlayer.getCurrentCharacter();
-                    event.getPlayer().sendMessage(ChatColor.YELLOW + character.getName() + "'s bio is " + ChatColor.GRAY + character.getBio());
+                    event.getPlayer().sendMessage(finalFormat);
+                    cooldown.add(event.getPlayer().getUniqueId());
+                    Bukkit.getScheduler().runTaskLaterAsynchronously(RPCore.i, () -> cooldown.remove(event.getPlayer().getUniqueId()), 20l);
                 }
             }
         }
