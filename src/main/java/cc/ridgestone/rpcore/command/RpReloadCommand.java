0c48078a75b0ff1fb739f18560db7e8d3c271214
Roby09 <q.r.depreter@gmail.com>
2023-07-30 21:51:08 +0200

    feat: rpreload command for reloading the config

---
+++ src/main/java/cc/ridgestone/rpcore/command/RpReloadCommand.java
@@ -0,0 +1,22 @@
+package cc.ridgestone.rpcore.command;
+
+import cc.ridgestone.rpcore.RPCore;
+import org.bukkit.ChatColor;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+
+public class RpReloadCommand implements CommandExecutor {
+
+    @Override
+    public boolean onCommand(CommandSender commandSender, Command command, String label, String[] arguments) {
+        if (!commandSender.hasPermission("rpcore.reload")) {
+            commandSender.sendMessage(ChatColor.RED + "You are not allowed to use this command");
+            return false;
+        }
+        RPCore.i.getConfigManager().loadConfig();
+        commandSender.sendMessage(ChatColor.GREEN + "RPCore config reloaded");
+        return false;
+    }
+
+}
